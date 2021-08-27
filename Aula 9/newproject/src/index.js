c993bafd48fc0df778a5a741844938ae7f8e5bcb
caiomanfredini <caiomanfredini.15@gmail.com>
2021-08-27 19:37:47 -0300

    fix(aula 7 e 8): renomear pasta para aula 7, 8 e 10

--- Aula 9/newproject/src/index.js
+++ Aula 9/newproject/src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App.js";
+// import App from "./App.js";
+import Calculator from "./Calculator.js";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Calculator />
   </React.StrictMode>,
   document.getElementById("root")
 );
